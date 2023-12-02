dfd44f2643dbe6106a25b42fd25b93d3cb1acde9
Bqnic <123201175+Bqnic@users.noreply.github.com>
2023-12-02 23:59:44 +0100

    navigation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react'
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="navigation">
+        <button>Home</button>
+        <button>Portfolio</button>
+        <button>Projects</button>
+        <button>Contact</button>
+      </div>
+    </>
+  );
+}
