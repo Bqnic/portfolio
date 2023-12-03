6c7825e1fa73e4e71dcab722737d9ef45b5b945a
Bqnic <123201175+Bqnic@users.noreply.github.com>
2023-12-03 21:15:58 +0100

    add smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 //import { useState } from 'react'
+import { Link } from "react-scroll";
 import "./App.css";
 
 export default function App() {
   return (
     <>
+      <div className="introduction">
+        <div>
+          <p>Hello!</p>
+          <p>
+            I'm <a>Stjepan Bonić.</a>
+          </p>
+        </div>
+        <div>
+          <p>An aspiring full stack developer.</p>
+        </div>
+        <Link
+          className="btn"
+          to="navigation"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+        >
+          Get to know me
+        </Link>
+      </div>
       <div className="navigation">
         <button>Home</button>
         <button>Portfolio</button>
