1c61d898011127e8c0b9dccd452684740865d81e
Bqnic <123201175+Bqnic@users.noreply.github.com>
2023-12-28 15:03:01 +0100

    improve desktop version for project section

--- src/assets/projects-helper.jsx
+++ src/assets/projects-helper.jsx
@@ -1,170 +1,196 @@
 export const projectArray = [];
 
 const knightTraversal = (
-  <div>
-    <p>Knight Traversal</p>
-    <p>
-      I was always <a>fascinated</a> by even the most simplest of algorithms, so
-      I decided I should make a simulation for one of them.
-    </p>
-    <p>
-      In this case, I made a <a>simulation for Breadth First Search (BFS)</a>,
-      where a Knight on a chessboard finds one of the shortest paths.
-    </p>
-    <p>
-      Made with: <a>HTML, CSS, JavaScript</a>.
-    </p>
-    <p>
-      Try it out:{" "}
-      <a href="https://bqnic.github.io/knight-traversal/" target="_blank">
-        Link
-      </a>
-    </p>
-    <p>
-      Code:{" "}
-      <a href="https://github.com/Bqnic/knight-traversal" target="_blank">
-        Link
-      </a>
-    </p>
-    <img src="../knightTraversal.gif" alt="" />
+  <div className="wrapper">
+    <h1>Knight Traversal</h1>
+    <div>
+      <div className="paras">
+        <p>
+          I was always <a>fascinated</a> by even the most simplest of
+          algorithms, so I decided I should make a simulation for one of them.
+        </p>
+        <p>
+          In this case, I made a{" "}
+          <a>simulation for Breadth First Search (BFS)</a>, where a Knight on a
+          chessboard finds one of the shortest paths.
+        </p>
+        <p>
+          Made with: <a>HTML, CSS, JavaScript</a>.
+        </p>
+        <p>
+          Try it out:{" "}
+          <a href="https://bqnic.github.io/knight-traversal/" target="_blank">
+            Link
+          </a>
+        </p>
+        <p>
+          Code:{" "}
+          <a href="https://github.com/Bqnic/knight-traversal" target="_blank">
+            Link
+          </a>
+        </p>
+      </div>
+      <img src="../knightTraversal.gif" alt="" />
+    </div>
   </div>
 );
 
 const battleship = (
-  <div>
-    <p>Battleship</p>
-    <p>
-      A classic game of Battleship where you play against a <a>computer bot</a>.
-    </p>
-    <p>
-      I tried my hand with <a>Test Driven Development</a> during this project
-      using <a>Jest</a>, it is not 100% tested, but for the inital development
-      it came in handy.
-    </p>
-    <p>
-      Made with: <a>HTML, CSS, JavaScript</a> and a little bit of <a>Jest</a>{" "}
-      for testing.
-    </p>
-    <p>
-      Try it out:{" "}
-      <a href="https://bqnic.github.io/battleship/" target="_blank">
-        Link
-      </a>
-    </p>
-    <p>
-      Code:{" "}
-      <a href="https://github.com/Bqnic/battleship" target="_blank">
-        Link
-      </a>
-    </p>
-    <img src="../battleship.gif" alt="" />
+  <div className="wrapper">
+    <h1>Battleship</h1>
+    <div>
+      <div className="paras">
+        <p>
+          A classic game of Battleship where you play against a{" "}
+          <a>computer bot</a>.
+        </p>
+        <p>
+          I tried my hand with <a>Test Driven Development</a> during this
+          project using <a>Jest</a>, it is not 100% tested, but for the inital
+          development it came in handy.
+        </p>
+        <p>
+          Made with: <a>HTML, CSS, JavaScript</a> and a little bit of{" "}
+          <a>Jest</a> for testing.
+        </p>
+        <p>
+          Try it out:{" "}
+          <a href="https://bqnic.github.io/battleship/" target="_blank">
+            Link
+          </a>
+        </p>
+        <p>
+          Code:{" "}
+          <a href="https://github.com/Bqnic/battleship" target="_blank">
+            Link
+          </a>
+        </p>
+      </div>
+      <img src="../battleship.gif" alt="" />
+    </div>
   </div>
 );
 
 const cvBuilder = (
-  <div>
-    <p>CV Builder</p>
-    <p>
-      I took upon this project to learn <a>React</a> and it was very useful to
-      grasp using <a>states</a> in React.
-    </p>
-    <p>
-      Made with: <a>React, CSS</a>.
-    </p>
-    <p>
-      Try it out:{" "}
-      <a href="https://cv-application-pi.vercel.app/" target="_blank">
-        Link
-      </a>
-    </p>
-    <p>
-      Code:{" "}
-      <a href="https://github.com/Bqnic/cv-application" target="_blank">
-        Link
-      </a>
-    </p>
-    <img src="../cvBuilder.png" alt="" />
+  <div className="wrapper">
+    <h1>CV Builder</h1>
+    <div>
+      <div className="paras">
+        <p>
+          I took upon this project to learn <a>React</a> and it was very useful
+          to grasp using <a>states</a> in React.
+        </p>
+        <p>
+          Made with: <a>React, CSS</a>.
+        </p>
+        <p>
+          Try it out:{" "}
+          <a href="https://cv-application-pi.vercel.app/" target="_blank">
+            Link
+          </a>
+        </p>
+        <p>
+          Code:{" "}
+          <a href="https://github.com/Bqnic/cv-application" target="_blank">
+            Link
+          </a>
+        </p>
+      </div>
+      <img src="../cvBuilder.png" alt="" />
+    </div>
   </div>
 );
 
 const schedulerGUI = (
-  <div>
-    <p>SchedulerGUI</p>
-    <p>
-      This was my very <a>first bigger project</a> which I opted to do in{" "}
-      <a>Java</a> since I didn't know JavaScript. For the GUI part, I used{" "}
-      <a>JavaFX library</a> coupled with SceneBuilder.
-    </p>
-    <p>
-      This was made to ease my organization of university studies, which I{" "}
-      <a>still use</a> in my day to day life.
-    </p>
-    <p>
-      Made with: <a>Java (JavaFX and SceneBuilder)</a>.
-    </p>
-    <p>
-      Code:{" "}
-      <a href="https://github.com/Bqnic/SchedulerGUI" target="_blank">
-        Link
-      </a>
-    </p>
-    <img src="../someTasks.png" alt="" />
+  <div className="wrapper">
+    <h1>SchedulerGUI</h1>
+    <div>
+      <div className="paras">
+        <p>
+          This was my very <a>first bigger project</a> which I opted to do in{" "}
+          <a>Java</a> since I didn't know JavaScript. For the GUI part, I used{" "}
+          <a>JavaFX library</a> coupled with SceneBuilder.
+        </p>
+        <p>
+          This was made to ease my organization of university studies, which I{" "}
+          <a>still use</a> in my day to day life.
+        </p>
+        <p>
+          Made with: <a>Java (JavaFX and SceneBuilder)</a>.
+        </p>
+        <p>
+          Code:{" "}
+          <a href="https://github.com/Bqnic/SchedulerGUI" target="_blank">
+            Link
+          </a>
+        </p>
+      </div>
+      <img src="../someTasks.png" alt="" />
+    </div>
   </div>
 );
 
 const todoList = (
-  <div>
-    <p>simply do</p>
-    <p>
-      A simple TODO list, it shares similarities to my SchedulerGUI project.
-    </p>
-    <p>
-      Main purpose of this project was to <a>see differences</a> between Java
-      and JavaScript when programming a very similar project.
-    </p>
-    <p>
-      Made with: <a>HTML, CSS, JavaScript</a>.
-    </p>
-    <p>
-      Try it out:{" "}
-      <a href="https://bqnic.github.io/todo-list/" target="_blank">
-        Link
-      </a>
-    </p>
-    <p>
-      Code:{" "}
-      <a href="https://github.com/Bqnic/todo-list" target="_blank">
-        Link
-      </a>
-    </p>
-    <img src="../todo.png" alt="" />
+  <div className="wrapper">
+    <h1>simply do</h1>
+    <div>
+      <div className="paras">
+        <p>
+          A simple TODO list, it shares similarities to my SchedulerGUI project.
+        </p>
+        <p>
+          Main purpose of this project was to <a>see differences</a> between
+          Java and JavaScript when programming a very similar project.
+        </p>
+        <p>
+          Made with: <a>HTML, CSS, JavaScript</a>.
+        </p>
+        <p>
+          Try it out:{" "}
+          <a href="https://bqnic.github.io/todo-list/" target="_blank">
+            Link
+          </a>
+        </p>
+        <p>
+          Code:{" "}
+          <a href="https://github.com/Bqnic/todo-list" target="_blank">
+            Link
+          </a>
+        </p>
+      </div>
+      <img src="../todo.png" alt="" />
+    </div>
   </div>
 );
 
 const weatherApp = (
-  <div>
-    <p>Weather App</p>
-    <p>
-      A pure form of <a>frontend project</a>, manipulating data from backend to
-      make it show up nicely on screen.
-    </p>
-    <p>I made this project to use and understand using API.</p>
-    <p>
-      Made with: <a>HTML, CSS, JavaScript</a>.
-    </p>
-    <p>
-      Try it out:{" "}
-      <a href="https://bqnic.github.io/weather-app/" target="_blank">
-        Link
-      </a>
-    </p>
-    <p>
-      Code:{" "}
-      <a href="https://github.com/Bqnic/weather-app" target="_blank">
-        Link
-      </a>
-    </p>
-    <img src="../weather.png" alt="" />
+  <div className="wrapper">
+    <h1>Weather App</h1>
+    <div>
+      <div className="paras">
+        <p>
+          A pure form of <a>frontend project</a>, manipulating data from backend
+          to make it show up nicely on screen.
+        </p>
+        <p>I made this project to use and understand using API.</p>
+        <p>
+          Made with: <a>HTML, CSS, JavaScript</a>.
+        </p>
+        <p>
+          Try it out:{" "}
+          <a href="https://bqnic.github.io/weather-app/" target="_blank">
+            Link
+          </a>
+        </p>
+        <p>
+          Code:{" "}
+          <a href="https://github.com/Bqnic/weather-app" target="_blank">
+            Link
+          </a>
+        </p>
+      </div>
+      <img src="../weather.png" alt="" />
+    </div>
   </div>
 );
 
